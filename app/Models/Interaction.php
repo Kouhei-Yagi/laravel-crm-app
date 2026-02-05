09ba79dbe1a69cd5f94c62f3a8417346bc8f1eee
Kouhei Yagi <artoflife19870310@gmail.com>
2026-02-05 11:06:07 +0900

    feat: Interaction モデルに$fillable を追加

--- app/Models/Interaction.php
+++ app/Models/Interaction.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Interaction extends Model
 {
-    //
+    protected $fillable = [
+        'customer_id',
+        'project_id',
+        'assigned_user_id',
+        'type',
+        'content',
+        'interacted_at',
+        'memo',
+    ];
 }
