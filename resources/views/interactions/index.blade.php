6957fd73cb8e4c22b8660ba72aad4d2ff14d5afa
Kouhei Yagi <artoflife19870310@gmail.com>
2026-02-10 10:14:04 +0900

    fix: 案件履歴一覧表示ページにおいてinteractions テーブルのproject_id/Customer_id カラムが null の場合にリンク生成でエラーになる問題を修正

--- resources/views/interactions/index.blade.php
+++ resources/views/interactions/index.blade.php
@@ -32,24 +32,37 @@
                                             {{ $interaction->interacted_at->format('Y-m-d H:i') }}
                                         </a>
                                     </td>
+
                                     <td class="px-3 py-2 border">
                                         {{ App\Models\Interaction::TYPE[$interaction->type] }}
                                     </td>
+
                                     <td class="px-3 py-2 border">
                                         {{ Str::limit($interaction->content, 30) }}
                                     </td>
+
                                     <td class="px-3 py-2 border">
-                                        <a href="{{ route('projects.show', $interaction->project) }}"
-                                            class="text-blue-600 hover:underline">
-                                            {{ optional($interaction->project)->title ?? '未設定' }}
-                                        </a>
+                                        @if ($interaction->project)
+                                            <a href="{{ route('projects.show', $interaction->project) }}"
+                                                class="text-blue-600 hover:underline">
+                                                {{ $interaction->project->title }}
+                                            </a>
+                                        @else
+                                            <span class="text-gray-400">未設定</span>
+                                        @endif
                                     </td>
+
                                     <td class="px-3 py-2 border">
-                                        <a href="{{ route('customers.show', $interaction->customer) }}"
-                                            class="text-blue-600 hover:underline">
-                                            {{ optional($interaction->customer)->name ?? '未設定' }}
-                                        </a>
+                                        @if ($interaction->customer)
+                                            <a href="{{ route('customers.show', $interaction->customer) }}"
+                                                class="text-blue-600 hover:underline">
+                                                {{ $interaction->customer->name }}
+                                            </a>
+                                        @else
+                                            <span class="text-gray-400">未設定</span>
+                                        @endif
                                     </td>
+
                                     <td class="px-3 py-2 border">
                                         {{ optional($interaction->user)->name ?? '未設定' }}
                                     </td>
