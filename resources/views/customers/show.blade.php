b199fa49247b7482fd3d7d4e43e6da7a6ecdcdfe
Kouhei Yagi <artoflife19870310@gmail.com>
2026-03-23 15:09:04 +0900

    refactoer: 顧客詳細画面のdanger ボタンをコンポーネントに置換

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -129,10 +129,9 @@
                         <form action="{{ route('customers.destroy', $customer) }}" method="post" class="inline-block">
                             @csrf
                             @method('delete')
-                            <button type="submit" onclick="return confirm('本当に削除しますか？')"
-                                class="inline-block px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 dark:hover:bg-red-500">
+                            <x-button.danger type="submit" onclick="return confirm('本当に削除しますか？')">
                                 削除
-                            </button>
+                            </x-button.danger>
                         </form>
 
                         <x-button.secondary href="{{ route('customers.index') }}">
