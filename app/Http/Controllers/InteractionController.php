f6d93b2f3916d625548db31e17c67e3289f069aa
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-08 15:20:40 +0900

    refactor: InteractionController@index の$assignedUsers を変更し、案件履歴一覧画面検索フォームの担当者欄をselect コンポーネントに置換

--- app/Http/Controllers/InteractionController.php
+++ app/Http/Controllers/InteractionController.php
@@ -24,7 +24,7 @@ class InteractionController extends Controller
         // 画面で選択肢として表示するため、対応種別・顧客名・担当者のデータを取得する
         $types = Interaction::TYPE;
         $customers = Customer::orderBy('kana')->pluck('name', 'id');
-        $assignedUsers = User::orderBy('name')->get();
+        $assignedUsers = User::orderBy('name')->pluck('name', 'id');
 
         // 検索条件をモデル側に集約し、コントローラーの責務を軽くするためにスコープを適用する
         $query = Interaction::query()
