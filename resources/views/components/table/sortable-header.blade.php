3ab3a716db7fc23c51956db46176d8ebb1ca2218
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-10 11:21:20 +0900

    refactor: sortable-header コンポーネントにソートリンク生成処理を追加

--- resources/views/components/table/sortable-header.blade.php
+++ resources/views/components/table/sortable-header.blade.php
@@ -3,7 +3,27 @@
     'column',
 ])
 
-<th class="px-3 py-2 border">
-    {{ $label }}
-</th>
+@php
+    $currentSort = request('sort');
+    $currentDirection = request('direction');
 
+    $nextDirection = ($currentSort === $column && $currentDirection === 'asc') ? 'desc' : 'asc';
+
+    $routeName = request()->route()->getName();
+
+    $url = route(
+        $routeName,
+        array_merge(
+            request()->query(), [
+                'sort' => $column,
+                'direction' => $nextDirection,
+            ])
+        );
+@endphp
+
+<th class="px-3 py-2 border">
+    <a href="{{ $url }}"
+        class="text-blue-600 dark:text-blue-400 hover:underline font-medium">
+        {{ $label }}
+    </a>
+</th>
