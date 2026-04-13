8633681840a6f93ef7e9ae3b27cbcc8446db7420
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-13 10:06:31 +0900

    feat: 顧客詳細画面に対応履歴一覧のセクションタイトルを追加

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -217,6 +217,10 @@
                         </table>
                     @endif
 
+                    {{-- 対応履歴一覧 --}}
+                    <h3 class="text-lg font-semibold mt-10 mb-3 text-gray-800 dark:text-gray-100">
+                        対応履歴一覧（{{ $customer->interactions->count() }}件）
+                    </h3>
                 </div>
             </div>
         </div>
