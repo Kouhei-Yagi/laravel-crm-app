0e45ee5d7f52a28ac2d9b6ecf3cd38873d25aa73
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-14 14:57:36 +0900

    refactor: 顧客詳細画面の案件一覧テーブルのタイトルをコンポーネント化

---
+++ resources/views/components/customer/project-list.blade.php
@@ -0,0 +1,3 @@
+<h3 class="text-lg font-semibold mt-10 mb-3 text-gray-800 dark:text-gray-100">
+    案件一覧（{{ $projects->count() }}件）
+</h3>
--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -141,9 +141,7 @@
                     </div>
 
                     {{-- 案件一覧 --}}
-                    <h3 class="text-lg font-semibold mt-10 mb-3 text-gray-800 dark:text-gray-100">
-                        案件一覧（{{ $projects->count() }}件）
-                    </h3>
+                    <x-customer.project-list :projects="$projects"/>
 
                     @if ($projects->isEmpty())
                         <p class="text-gray-500 dark:text-gray-400">案件はありません。</p>
