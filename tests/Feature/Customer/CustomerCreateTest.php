f913432341568e22c607680b8f4cc3c1cb4718ee
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-22 09:26:54 +0900

    test: CustomerCreateTest に入力必須項目のバリデーションエラーのテストを追加

--- tests/Feature/Customer/CustomerCreateTest.php
+++ tests/Feature/Customer/CustomerCreateTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use Test\Tests\TestCase;
+use Tests\TestCase;
 
 it('ログインユーザーの顧客作成画面の表示テスト', function () {
     // ログインユーザーを作成してログイン
@@ -10,7 +10,7 @@ it('ログインユーザーの顧客作成画面の表示テスト', function (
     $response = $this->get('/customers/create');
 
     // ログイン成功（ステータスコード 200）を確認
-    $response->assertOk('/customers/create');
+    $response->assertOk();
 });
 
 it('customers テーブルへの新規登録テスト', function () {
@@ -18,14 +18,14 @@ it('customers テーブルへの新規登録テスト', function () {
     $this->loginUser();
 
     // 送信するデータ（Customer の入力フォーム）
-    $date = [
+    $data = [
         'name' => '山田 太郎',
         'status' => 'prospect',
         'rank' => 'A',
     ];
 
     // POST /customers にデータを送信
-    $response = $this->post('/customers', $date);
+    $response = $this->post('/customers', $data);
 
     // 正常にリダイレクトされることを確認
     $response->assertRedirect('/customers');
@@ -36,4 +36,67 @@ it('customers テーブルへの新規登録テスト', function () {
         'status' => 'prospect',
         'rank' => 'A',
     ]);
+});
+
+it('名前のバリデーションエラーのテスト', function () {
+    // ログインユーザーを作成してログイン
+    $this->loginUser();
+
+    // 送信するデータ（Customer の入力フォーム）
+    $data = [
+        'name' => '',
+        'status' => 'prospect',
+        'rank' => 'A',
+    ];
+
+    // POST /customers にデータを送信
+    $response = $this->post('/customers', $data);
+
+    // バリデーションエラー → 302 リダイレクトを確認
+    $response->assertStatus(302);
+
+    // name にエラーがあることを確認
+    $response->assertSessionHasErrors(['name']);
+});
+
+it('ステータスのバリデーションエラーのテスト', function () {
+    // ログインユーザーを作成してログイン
+    $this->loginUser();
+
+    // 送信するデータ（Customer の入力フォーム）
+    $data = [
+        'name' => '山田 太郎',
+        'status' => '',
+        'rank' => 'A',
+    ];
+
+    // POST / customers にデータ送信
+    $response = $this->post('/customers', $data);
+
+    // バリデーションエラー → 302 リダイレクトを確認
+    $response->assertStatus(302);
+
+    // status にエラーがあることを確認
+    $response->assertSessionHasErrors(['status']);
+});
+
+it('ランクのバリデーションエラーのテスト', function () {
+    // ログインユーザーを作成してログイン
+    $this->loginUser();
+
+    // 送信するデータ（Customer の入力フォーム）
+    $data = [
+        'name' => '山田 太郎',
+        'status' => 'prospect',
+        'rank' => '',
+    ];
+
+    // POST / customers にデータ送信
+    $response = $this->post('/customers', $data);
+
+    // バリデーションエラー → 302 リダイレクを確認
+    $response->assertStatus(302);
+
+    // rank にエラーがあることを確認
+    $response->assertSessionHasErrors(['rank']);
 });
