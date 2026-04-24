fad82ae0cf0141d0929f1dd6322e96c53efa21c8
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-25 08:51:05 +0900

    test: CustomerUpdateRequestTest に入力必須項目（name / status / rank）バリデーション単体テストを追加

---
+++ tests/Unit/Requests/CustomerUpdateRequestTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Http\Requests\CustomerUpdateRequest;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+uses(TestCase::class);
+
+it('name は必須である', function () {
+    // 入力フォームから送信するデータ
+    $data = [
+        'name' => '',
+        'status' => 'prospect',
+        'rank' => 'A',
+    ];
+
+    // CustomerUpdateRequest の rules を取得
+    $rules = (new CustomerUpdateRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションエラーになることを確認
+    expect($validator->fails())->toBeTrue();
+});
+
+it('status は必須である', function () {
+    // 入力フォームから送信するデータ
+    $data = [
+        'name' => '山田 太郎',
+        'status' => '',
+        'rank' => 'A',
+    ];
+
+    // CustomerUpdateRequest の rules の取得
+    $rules = (new CustomerUpdateRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションエラーになることを確認
+    expect($validator->fails())->toBeTrue();
+});
+
+it('rank は必須である', function () {
+    // 入力フォームから送信するデータ
+    $data = [
+        'name' => '山田 太郎',
+        'status' => 'prospect',
+        'rank' => '',
+    ];
+
+    // CustomerUpdateRequest の rules を取得
+    $rules = (new CustomerUpdateRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションエラーになることを確認
+    expect($validator->fails())->toBeTrue();
+});
