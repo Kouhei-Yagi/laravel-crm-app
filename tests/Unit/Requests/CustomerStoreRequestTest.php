778ac0685d12bb33a6f50726f2bdf83d307d828f
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-24 10:24:04 +0900

    test: CustomerStoreRequestTest にメール形式バリデーション単体テスト（valid / invalid）を追加

--- tests/Unit/Requests/CustomerStoreRequestTest.php
+++ tests/Unit/Requests/CustomerStoreRequestTest.php
@@ -62,3 +62,58 @@ it('顧客新規登録処理の rank は必須である', function () {
     // バリデーションエラーになることを確認
     expect($validator->fails())->toBeTrue();
 });
+
+it('顧客新規登録処理の email は正しい形式ならバリデーションを通過する', function ($validEmail) {
+    // 入力フォームからの送信データ
+    $data = [
+        'name' => '山田 太郎',
+        'status' => 'prospect',
+        'rank' => 'A',
+        'email' => $validEmail,
+    ];
+
+    // CustomerStoreRequest の rules を取得
+    $request = new CustomerStoreRequest();
+    $rules = $request->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーション成功を確認
+    expect($validator->fails())->toBeFalse();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    'test@example.com',
+    'user.name@example.co.jp',
+    'user_name@example.net',
+    'user-name@example.org',
+]);
+
+it('顧客新規登録処理の email は不正な形式だとバリデーションエラーになる', function ($invalidEmail) {
+    // 入力フォームから送信するデータ
+    $data = [
+        'name' => '山田 太郎',
+        'status' => 'prospect',
+        'rank' => 'A',
+        'email' => $invalidEmail,
+    ];
+
+    // CustomerStoreRequest の rules を取得
+    $request = new CustomerStoreRequest();
+    $rules = $request->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションエラーになることを確認
+    expect($validator->fails())->toBeTrue();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    'test',              // @なし
+    'test@',             // ドメインなし
+    '@example.com',      // ローカルパートなし
+    'test@@example.com', // @が2つ
+    'test@ example.com', // スペース入り
+]);
