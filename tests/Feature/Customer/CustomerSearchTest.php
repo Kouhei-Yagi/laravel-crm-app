a1c50c9b38be2b0521b1cf24f6edac2e7cbfcee2
Kouhei Yagi <artoflife19870310@gmail.com>
2026-04-26 11:05:13 +0900

    test: CustomerSearchTest に email の部分一致・完全一致検索テストを追加

--- tests/Feature/Customer/CustomerSearchTest.php
+++ tests/Feature/Customer/CustomerSearchTest.php
@@ -11,7 +11,7 @@ it('name の部分一致検索ができる', function () {
     Customer::factory()->create(['name' => '山田 太郎']);
     Customer::factory()->create(['name' => '佐藤 花子']);
 
-    // '?name=山田' で検索処理にアクセス
+    // '?keyword=山田' で検索処理にアクセス
     $response = $this->get('/customers?keyword=山田');
 
     // アクセス成功（ステータスコード 200）を確認
@@ -22,4 +22,33 @@ it('name の部分一致検索ができる', function () {
 
     // 名前が表示されないことを確認
     $response->assertDontSee('佐藤 花子');
+});
+
+it('email の部分一致・完全一致検索ができる', function () {
+    // ログインユーザーを作成し、ログイン状態にする
+    $this->loginUser();
+
+    // 検索にヒットする任意の顧客を作成
+    Customer::factory()->create(['email' => 'test@example.com']);
+    // 検索にヒットしない任意の顧客を作成
+    Customer::factory()->create(['email' => 'other@example.com']);
+
+    // 完全一致
+    // '?keyword=test@example.com' で検索処理にアクセス
+    $response = $this->get('/customers?keyword=test@example.com');
+    // アクセス成功（ステータスコード 200）を確認
+    $response->assertOk();
+    // 検索条件にヒットしたデータが表示されることを確認
+    $response->assertSee('test@example.com');
+    // 検索条件にヒットしないデータは表示されないことを確認
+    $response->assertDontSee('other@example.com');
+
+    // 部分一致
+    // '?keyword=@example.com' で検索処理にアクセス
+    $response = $this->get('/customers?keyword=@example.com');
+    // アクセス成功（ステータスコード 200）を確認
+    $response->assertOk();
+    // 検索条件にヒットしたデータが表示されることを確認
+    $response->assertSee('test@example.com');
+    $response->assertSee('other@example.com');
 });
