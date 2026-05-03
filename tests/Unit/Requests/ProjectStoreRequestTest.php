da7b7d5c2e48214b7177772f0ffd9922ca18d96f
Kouhei Yagi <artoflife19870310@gmail.com>
2026-05-03 14:14:57 +0900

    test: ProjectStoreRequestTest に start_date / end_date の形式テスト（valid / invalid）を追加

--- tests/Unit/Requests/ProjectStoreRequestTest.php
+++ tests/Unit/Requests/ProjectStoreRequestTest.php
@@ -132,3 +132,129 @@ it('amount が不正な形式の場合はバリデーションエラーになる
     -1,
     -100,
 ]);
+
+it('start_date が正しい形式の場合はバリデーションを通過する', function ($validStartDate) {
+    // 任意の顧客を作成
+    $customer = Customer::factory()->create();
+
+    // 送信するデータ（検索フォームに入力されたデータ）を作成
+    $data = [
+        'title' => 'ホームページ制作',
+        'customer_id' => $customer->id,
+        'status' => 'estimating',
+        'start_date' => $validStartDate,
+    ];
+
+    // バリデーションルールを取得
+    $rules = (new ProjectStoreRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションを通過することを確認
+    expect($validator->fails())->toBeFalse();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    null,
+    '',
+    '2024-01-01',
+    '2025-12-31',
+    '2024/01/01',
+    'January 1, 2024',
+]);
+
+it('start_date が不正な形式の場合はバリデーションエラーになる', function ($invalidStartDate) {
+    // 任意の顧客を作成
+    $customer = Customer::factory()->create();
+
+    // 送信するデータ（検索フォームに入力されたデータ）を作成
+    $data = [
+        'title' => 'ホームページ制作',
+        'customer_id' => $customer->id,
+        'status' => 'estimating',
+        'start_date' => $invalidStartDate,
+    ];
+
+    // バリデーションルールを取得
+    $rules = (new ProjectStoreRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションを通過することを確認
+    expect($validator->fails())->toBeTrue();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    'abc',
+    '2024-13-01',
+    '2024-00-10',
+    '2024-01-32',
+    '2024-02-30',
+    '2024/13/01',
+    12345,
+]);
+
+it('end_date が正しい形式の場合はバリデーションを通過する', function ($validEndDate) {
+    // 任意の顧客を作成
+    $customer = Customer::factory()->create();
+
+    // 送信するデータ（検索フォームに入力されたデータ）を作成
+    $data = [
+        'title' => 'ホームページ制作',
+        'customer_id' => $customer->id,
+        'status' => 'estimating',
+        'end_date' => $validEndDate,
+    ];
+
+    // バリデーションルールを取得
+    $rules = (new ProjectStoreRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションを通過することを確認
+    expect($validator->fails())->toBeFalse();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    null,
+    '',
+    '2024-01-01',
+    '2025-12-31',
+    '2024/01/01',
+    'January 1, 2024',
+]);
+
+it('end_date が不正な形式の場合はバリデーションエラーになる', function ($invalidEndDate) {
+    // 任意の顧客を作成
+    $customer = Customer::factory()->create();
+
+    // 送信するデータ（検索フォームに入力されたデータ）を作成
+    $data = [
+        'title' => 'ホームページ制作',
+        'customer_id' => $customer->id,
+        'status' => 'estimating',
+        'end_date' => $invalidEndDate,
+    ];
+
+    // バリデーションルールを取得
+    $rules = (new ProjectStoreRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションを通過することを確認
+    expect($validator->fails())->toBeTrue();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    'abc',
+    '2024-13-01',
+    '2024-00-10',
+    '2024-01-32',
+    '2024-02-30',
+    '2024/13/01',
+    12345,
+]);
