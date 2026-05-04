6d09ae219aeb2b0fd7485806bec315009090d4a4
Kouhei Yagi <artoflife19870310@gmail.com>
2026-05-04 09:28:41 +0900

    test: ProjectUpdateRequestTest に amount の形式テスト（valid / invalid）を追加

--- tests/Unit/Requests/ProjectUpdateRequestTest.php
+++ tests/Unit/Requests/ProjectUpdateRequestTest.php
@@ -40,3 +40,59 @@ it('status がない場合はバリデーションエラーになる', function 
     // バリデーションエラーになることを確認
     expect($validator->fails())->toBeTrue();
 });
+
+it('amount が正しい形式の場合はバリデーションを通過する', function ($validAmount) {
+    // 送信するデータ（案件編集フォームに入力されたデータ）を作成
+    $data = [
+        'title' => 'ホームページ制作',
+        'status' => 'estimating',
+        'amount' => $validAmount,
+    ];
+
+    // バリデーションルールを取得
+    $rules = (new ProjectUpdateRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションを通過することを確認
+    expect($validator->fails())->toBeFalse();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    null,
+    '',
+    0,
+    1,
+    '0',
+    '1',
+]);
+
+it('amount が不正な形式の場合はバリデーションエラーになる', function ($invalidAmount) {
+    // 送信するデータ（案件作成フォームから入力されたデータ）を作成
+    $data = [
+        'title' => 'ホームページ制作',
+        'status' => 'estimating',
+        'amount' => $invalidAmount,
+    ];
+
+    // バリデーションルールを取得
+    $rules = (new ProjectUpdateRequest())->rules();
+
+    // バリデーション実行
+    $validator = Validator::make($data, $rules);
+
+    // バリデーションエラーになることを確認
+    expect($validator->fails())->toBeTrue();
+
+    // データプロバイダ機能で繰り返す
+})->with([
+    'abc',
+    '100abc',
+    '１２３',
+    '1.5',
+    1.5,
+    '1,000',
+    -1,
+    -100,
+]);
