a31015e159f9ac34f1ea50963212b14bd071acd1
Kouhei Yagi <artoflife19870310@gmail.com>
2026-05-07 09:17:49 +0900

    test: ProjectSortTest に created_at の昇順・降順ソートテストを追加

--- tests/Feature/Project/ProjectSortTest.php
+++ tests/Feature/Project/ProjectSortTest.php
@@ -217,3 +217,77 @@ it('ログインユーザーは amount で降順ソートができる', function
     // 案件名が降順にソートされて表示されることを確認
     $response->assertSeeInOrder(['C案件', 'B案件', 'A案件']);
 });
+
+it('ログインユーザーは created_at で昇順ソートができる', function () {
+    // ログインユーザーを作成し、ログイン状態にする
+    $this->loginUser();
+
+    // 任意の顧客を作成
+    $customer = Customer::factory()->create();
+
+    // 顧客に紐づく並び順がバラバラの案件を3件作成
+    Project::factory()->create([
+        'customer_id' => $customer->id,
+        'created_at' => '2026-03-01',
+        'title' => 'C案件',
+    ]);
+    Project::factory()->create([
+        'customer_id' => $customer->id,
+        'created_at' => '2026-01-01',
+        'title' => 'A案件',
+    ]);
+    Project::factory()->create([
+        'customer_id' => $customer->id,
+        'created_at' => '2026-02-01',
+        'title' => 'B案件',
+    ]);
+
+    // ソート処理にアクセス
+    $response = $this->get(route('projects.index', [
+        'sort' => 'created_at',
+        'direction' => 'asc',
+    ]));
+
+    // アクセス成功（ステータスコード 200）を確認
+    $response->assertOk();
+
+    // 案件名が昇順にソートされて表示されることを確認
+    $response->assertSeeInOrder(['A案件', 'B案件', 'C案件']);
+});
+
+it('ログインユーザーは created_at で降順ソートができる', function () {
+    // ログインユーザーを作成し、ログイン状態にする
+    $this->loginUser();
+
+    // 任意の顧客を作成
+    $customer = Customer::factory()->create();
+
+    // 顧客に紐づく並び順がバラバラの案件を3件作成
+    Project::factory()->create([
+        'customer_id' => $customer->id,
+        'created_at' => '2026-01-01',
+        'title' => 'A案件',
+    ]);
+    Project::factory()->create([
+        'customer_id' => $customer->id,
+        'created_at' => '2026-03-01',
+        'title' => 'C案件',
+    ]);
+    Project::factory()->create([
+        'customer_id' => $customer->id,
+        'created_at' => '2026-02-01',
+        'title' => 'B案件',
+    ]);
+
+    // ソート処理にアクセス
+    $response = $this->get(route('projects.index', [
+        'sort' => 'created_at',
+        'direction' => 'desc',
+    ]));
+
+    // アクセス成功（ステータスコード 200）を確認
+    $response->assertOk();
+
+    // 案件名が降順にソートされて表示されることを確認
+    $response->assertSeeInOrder(['C案件', 'B案件', 'A案件']);
+});
