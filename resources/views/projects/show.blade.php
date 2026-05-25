31263e87cada0c9641a47d887073f84dd2747f42
Kouhei Yagi <artoflife19870310@gmail.com>
2026-05-25 14:58:44 +0900

    style: 詳細ページの UI を統一（カード外枠に border を追加）

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -29,7 +29,8 @@
 
     <div class="py-6">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
+            <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-600 shadow-sm rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
                     {{-- フラッシュメッセージ --}}
@@ -88,6 +89,7 @@
 
                 </div>
             </div>
+
         </div>
     </div>
 </x-app-layout>
